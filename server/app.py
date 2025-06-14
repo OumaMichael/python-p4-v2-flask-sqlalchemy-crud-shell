8b3e416adacf597dbafdac61d76c294ee9d1e102
Michael Ouma <Oumamichael108@gmail.com>
2025-06-15 02:20:59 +0300

    Completed Earthquake model and routes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Pet
 
 # create a Flask application instance 
 app = Flask(__name__)
@@ -20,6 +20,10 @@ migrate = Migrate(app, db)
 # initialize the Flask application to use the database
 db.init_app(app)
 
+
+@app.route('/')
+def home():
+    return "Flask-SQLAlchemy CRUD Code-Along"
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
